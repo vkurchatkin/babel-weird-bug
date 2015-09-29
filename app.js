c2173b36109bdddb3a7c28a37a96d3accc196025
Vladimir Kurchatkin <vladimir.kurchatkin@gmail.com>
2015-09-29 20:01:36 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+import { Cls } from './lib.js';
+
+// й <-- the fun part
+
+class Application {
+  run() {
+    new Cls();
+  }
+}
+
+export default Application;
---
+++ main.js
@@ -0,0 +1,3 @@
+import Application from './App.js'
+
+new Application().run();
